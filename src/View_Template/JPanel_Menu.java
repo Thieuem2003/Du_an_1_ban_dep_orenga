a0a85d3c88d3a7b2ce073400a031918efabe6625
TRAMPH23152 <tramcvph23152@fpt.edu>
2024-01-08 09:29:57 +0700

    Update Form_GiaoDien TRAM_PH23152  1/8/2024

--- src/View_Template/JPanel_Menu.java
+++ src/View_Template/JPanel_Menu.java
@@ -60,7 +60,7 @@ public class JPanel_Menu extends javax.swing.JPanel {
     protected void paintChildren(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        GradientPaint g1 = new GradientPaint(0, 0, Color.decode("#FFFF99"), 0, getHeight(), Color.decode("#000046"));
+        GradientPaint g1 = new GradientPaint(0, 0, Color.decode("#FFFF99"), 0, getHeight(), Color.decode("#FFA500"));
         g2.setPaint(g1);
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
         g2.fillRect(getWidth() - 20, 0, getWidth(), getHeight());
